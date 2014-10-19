1f3d49feddfb5c94f5dd306ebd9bf338d5c6b0ae
Derrick Showers <derrick@derrickshowers.com>
2014-10-18 22:51:42 -0700

    Initial Commit

---
+++ RecommendIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecommendIt
+//
+//  Created by Derrick Showers on 10/18/14.
+//  Copyright (c) 2014 Derrick Showers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
